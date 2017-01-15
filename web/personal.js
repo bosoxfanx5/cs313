2d33871231078e2f00c34246e0ac6cc785d25e4d
bosoxfanx5 <rob15047@byui.edu>
2017-01-14 20:32:49 -0800

    final javascript doc

--- web/personal.js
+++ web/personal.js
@@ -1,26 +1,18 @@
-$(document).ready(function(){
-    // Activate Carousel
-    $("#myCarousel").carousel();
 
-    // Enable Carousel Indicators
-    $(".item1").click(function(){
-        $("#myCarousel").carousel(0);
-    });
-    $(".item2").click(function(){
-        $("#myCarousel").carousel(1);
-    });
-    $(".item3").click(function(){
-        $("#myCarousel").carousel(2);
-    });
-    $(".item4").click(function(){
-        $("#myCarousel").carousel(3);
-    });
+$(document).ready(function() {
+	
+	$(".aboutme.hidden").fadeIn(2000).removeClass('hidden');
 
-    // Enable Carousel Controls
-    $(".left").click(function(){
-        $("#myCarousel").carousel("prev");
-    });
-    $(".right").click(function(){
-        $("#myCarousel").carousel("next");
-    });
+	$(".email").click(function() {
+		$(".emailForm").fadeIn(2000).removeClass('hidden');
+	
+	});
+	
+	$("#subBtn").click(function() {
+		$(".emailForm").addClass('hidden');
+		alert("This was my attempt at setting up an email. I hope to learn this technique in class!");
+	
+	
+	});
+
 });
