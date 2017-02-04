392761472f7bab2940f1c5498dcfe746e1c67f2e
bosoxfanx5 <rob15047@byui.edu>
2017-02-03 20:58:32 -0800

    Add files via upload

--- web/Project/mobile.php
+++ web/Project/mobile.php
@@ -162,10 +162,10 @@ _/          _/_/      _/_/        _/      _/_/_/_/  _/    _/
 
 <!-- Begin footer content -->
 <footer class="footer">
-	<nav class="navbar navbar-default" role="navigation">
+	<nav class="navbar navbar-default">
 		<!-- Brand and toggle get grouped for better mobile display -->
 		<div class="navbar-header">
-			<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar-collapse-1">
+			<button type="button">
 				<span class="sr-only">Toggle navigation</span>
 				<span class="icon-bar"></span>
 				<span class="icon-bar"></span>
