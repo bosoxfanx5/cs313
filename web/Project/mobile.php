6f7a085b955591ac04cafa48b72cbe16e565c111
bosoxfanx5 <rob15047@byui.edu>
2017-02-03 23:39:02 -0800

    Add files via upload

--- web/Project/mobile.php
+++ web/Project/mobile.php
@@ -148,22 +148,13 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
 	<?php
       if (!$isContent) {
           if (!empty($result)) {
-            foreach($result as $row) {
-              print_r('<strong><a href="mobile.php?id='
-                        . $row["id"]          . '">'
-                        . $row["title"]       . " "
-                        . $row["description"] . ":"
-                        . $row["title"]       .
-                      '</a></strong><br><br>');
-            }
-          }
-        } else {
+
           print_r('<strong>' . $result["title"]       . " "
                              . $result["description"] . ":"
                              . $result["title"]       . "</strong> - "
                              . $result['title']);
-			echo "This section is active";
         }
+	  }
       ?>
 </div>
 
