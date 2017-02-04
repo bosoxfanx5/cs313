a465e96ea382287ebc2952845ffae6845dea87ef
bosoxfanx5 <rob15047@byui.edu>
2017-02-03 20:56:10 -0800

    Add files via upload

--- web/Project/mobile.php
+++ web/Project/mobile.php
@@ -191,6 +191,7 @@ _/          _/_/      _/_/        _/      _/_/_/_/  _/    _/
 ================================================== -->
 <!-- Placed at the end of the document so the pages load faster -->
 <!-- Latest compiled and minified JavaScript -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
