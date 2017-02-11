f91d570ac910a1f2069eaa15053f72010978f350
bosoxfanx5 <rob15047@byui.edu>
2017-02-10 22:21:21 -0800

    Add files via upload

--- web/Project/login.php
+++ web/Project/login.php
@@ -124,16 +124,14 @@ Heroku CLI: heroku pg:psql postgresql-cubic-94519 --app rocky-everglades-86262
 		</form>
 	</div>
 </nav>
+
 <!-- Begin page content -->
 <div class="mycontainer">
 <div class="wrapper">
    <form class="form-signin">       
    	<h2 class="form-signin-heading">Please login</h2>
-      <input type="text" class="form-control" name="username" placeholder="Email Address" required="" autofocus="" />
-      <input type="password" class="form-control" name="password" placeholder="Password" required=""/>      
-      <label class="checkbox">
-      <input type="checkbox" value="remember-me" id="rememberMe" name="rememberMe"> Remember me
-      </label>
+      <input type="text" class="form-control" name="username" placeholder="Email Address" />
+      <input type="password" class="form-control" name="password" placeholder="Password" />
    	<button class="btn btn-lg btn-primary btn-block" type="submit">Login</button>   
 	</form>
 </div>
