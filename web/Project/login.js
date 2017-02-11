1f937bf9492568cedf623b47f29f2d23c779da12
bosoxfanx5 <rob15047@byui.edu>
2017-02-11 15:22:02 -0800

    Add files via upload

--- web/Project/login.js
+++ web/Project/login.js
@@ -1,23 +1,21 @@
-$(document).ready(function() {
-	$("forgot").click(function() {
-		$("setPass").style.visibility = visible;
+$("forgot").click(function() {
+	$("setPass").style.visibility = visible;
 
 
 
-	};
+};
 	
-	$("close").click(function() {
-		$("setPass").style.visibility = hidden;
+$("close").click(function() {
+	$("setPass").style.visibility = hidden;
 
 
 
-	};
+};
 	
-	$("save").click(function() {
-		$("setPass").style.visibility = hidden;
+$("save").click(function() {
+	$("setPass").style.visibility = hidden;
 
 
 
-	};
 };
 
