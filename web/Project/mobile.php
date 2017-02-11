9df24bdf3dcac1987b6f7321f699c184b4df0d82
bosoxfanx5 <rob15047@byui.edu>
2017-02-11 14:20:41 -0800

    Add files via upload

--- web/Project/mobile.php
+++ web/Project/mobile.php
@@ -102,7 +102,7 @@ $database = null;
 
 
 <!-- Fixed navbar -->
-<nav class="navbar navbar-default navbar-fixed-top" role="navigation">
+<nav class="navbar navbar-default" role="navigation">
 	<!-- Brand and toggle get grouped for better mobile display -->
 	<div class="navbar-header">
 		<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar-collapse-1">
