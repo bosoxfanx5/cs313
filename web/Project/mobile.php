dbe25911c41965bf2a340c17ce30e0544dd865b6
bosoxfanx5 <rob15047@byui.edu>
2017-02-11 18:04:56 -0800

    Add files via upload

--- web/Project/mobile.php
+++ web/Project/mobile.php
@@ -138,7 +138,7 @@ $database = null;
 				</ul>
 			</li>
 			<?php if (isset($_POST["email"])) : ?>
-				<?php echo "<li>$email</li>"; ?>
+				<li><a href="#"><?php echo $_POST["email"]; ?></a></li>
 			<?php else : ?>
 				<li><a href="login.php">Login</a></li>
 			<?php endif?>
