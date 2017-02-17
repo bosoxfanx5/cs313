d464170ff811374ff4d813e3c6b7d8b45a7f3056
bosoxfanx5 <rob15047@byui.edu>
2017-02-16 23:32:19 -0800

    Add files via upload

--- web/Project/mobile.php
+++ web/Project/mobile.php
@@ -130,7 +130,7 @@ $database = null;
 					?>
 				</ul>
 			</li>
-			<?php if ($_SESSION["isLoggedIn"]) : ?>
+			<?php if (isset($_SESSION["email"])) : ?>
 				<li><a href="#"><?php echo $_SESSION["email"]; ?></a></li>
 			<?php else : ?>
 				<li><a href="login.php">Login</a></li>
@@ -189,9 +189,43 @@ _/_/_/      _/_/    _/_/_/        _/
 	<?php endif ?>
 <?php endif ?>
 </div>
-</body>
+!--
+_/_/_/_/    _/_/      _/_/    _/_/_/_/_/  _/_/_/_/  _/_/_/
+_/        _/    _/  _/    _/      _/      _/        _/    _/
+_/_/_/    _/    _/  _/    _/      _/      _/_/_/    _/_/_/
+_/        _/    _/  _/    _/      _/      _/        _/    _/
+_/          _/_/      _/_/        _/      _/_/_/_/  _/    _/
+-->
+<!-- Begin footer content -->
+<footer class="footer">
+	<nav class="navbar navbar-default">
+		<!-- Brand and toggle get grouped for better mobile display -->
+		<div class="navbar-header">
+			<!-- Left Side -->
+			<div class="btn-group">
+				<ul class="nav navbar-nav navbar-left">
+					&nbsp;
+					<li><a href="#">Copyright 2017 Brooks Robison, All Rights Reserved</a></li>
+				</ul>
+			</div>
+		</div>
+		<!-- Center -->
+		<div class="navbar-center navbar-brand" href="#"><a class="navbar-brand"></a></div>
+		<!-- Collect the nav links, forms, and other content for toggling -->
 
-<?php
-include 'footer.php';
-?>
+	</nav>
+</footer>
+
+
+<!-- Bootstrap core JavaScript
+================================================== -->
+<!-- Placed at the end of the document so the pages load faster -->
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+<!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+<!-- <script src="../../assets/js/ie10-viewport-bug-workaround.js"></script> -->
+</body>
+</body>
 </html>
