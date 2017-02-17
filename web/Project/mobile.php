030b9eca5c644e33d63e8bfef2bcdf5597b3e546
bosoxfanx5 <rob15047@byui.edu>
2017-02-16 18:17:07 -0800

    Add files via upload

--- web/Project/mobile.php
+++ web/Project/mobile.php
@@ -11,6 +11,7 @@ Heroku CLI: heroku pg:psql postgresql-cubic-94519 --app rocky-everglades-86262--
 session_start();
 include 'head.php';
 include 'header.php';
+include 'footer.php';
 error_reporting(E_ALL);
 ini_set("display_errors", 1);
 
@@ -77,11 +78,6 @@ $database = null;
 <html lang="en">
 
 <body>
-
-
-<!-- Fixed navbar -->
-
-
 <!--
 _/_/_/      _/_/    _/_/_/    _/      _/
 _/    _/  _/    _/  _/    _/    _/  _/
@@ -131,43 +127,5 @@ _/_/_/      _/_/    _/_/_/        _/
 			</div>
 		<?php endif ?>
 	<?php endif ?>
-	<!--
-	_/_/_/_/    _/_/      _/_/    _/_/_/_/_/  _/_/_/_/  _/_/_/
-	_/        _/    _/  _/    _/      _/      _/        _/    _/
-	_/_/_/    _/    _/  _/    _/      _/      _/_/_/    _/_/_/
-	_/        _/    _/  _/    _/      _/      _/        _/    _/
-	_/          _/_/      _/_/        _/      _/_/_/_/  _/    _/
--->
-</div>
-<!-- Begin footer content -->
-<footer class="footer">
-	<nav class="navbar navbar-default">
-		<!-- Brand and toggle get grouped for better mobile display -->
-		<div class="navbar-header">
-			<!-- Left Side -->
-			<div class="btn-group">
-				<ul class="nav navbar-nav navbar-left">
-					&nbsp;
-					<li><a href="#">Copyright 2017 Brooks Robison, All Rights Reserved</a></li>
-				</ul>
-			</div>
-		</div>
-		<!-- Center -->
-		<div class="navbar-center navbar-brand" href="#"><a class="navbar-brand"></a></div>
-		<!-- Collect the nav links, forms, and other content for toggling -->
-
-	</nav>
-</footer>
-
-
-<!-- Bootstrap core JavaScript
-================================================== -->
-<!-- Placed at the end of the document so the pages load faster -->
-<!-- Latest compiled and minified JavaScript -->
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-<!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
-<!-- <script src="../../assets/js/ie10-viewport-bug-workaround.js"></script> -->
 </body>
 </html>
