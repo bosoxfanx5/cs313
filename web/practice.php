24487cc813df5c9d6e7a1dff067917f2740584eb
bosoxfanx5 <rob15047@byui.edu>
2017-02-22 23:21:32 -0800

    Update practice.php

--- web/practice.php
+++ web/practice.php
@@ -1,95 +1,103 @@
 <?php
-error_reporting(E_ALL);
-ini_set("display_errors", 0);
-if (isset($_POST['submit'])) {
-   $error = false;
-   $name = $_POST['name'];
-   $to = $_POST['email'];
-   $message = $_POST['message'];
-
-   if (!$_POST['name']) {
-      $errorName = "Please provide your name";
-      $error = true;
-   }
-
-   if (!$_POST['email'] || !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
-      $errorEmail = "Please provide your email address";
-      $error = true;
-   }
-
-   if (!$_POST['message']) {
-      $errorMessage = "Please include a message";
-      $error = true;
-   }
-
-   $from = "Ballotpedia";
-   $subject = "Contact Form Email";
-
-   if (!$errorName && !$errorEmail && !$errorMessage) {
-      if (true) {
-         $result = "<div class='alert alert-success'>Thank you! We'll be in contact shortly!</div>";
-      } else {
-         $result = "<div class='alert alert-danger'>We're sorry, but your email was unable to be sent.</div>";
-      }
-   }
+	if (isset($_POST["submit"])) {
+		$name = $_POST['name'];
+		$email = $_POST['email'];
+		$message = $_POST['message'];
+		$human = intval($_POST['human']);
+		$from = 'Demo Contact Form'; 
+		$to = 'example@domain.com'; 
+		$subject = 'Message from Contact Demo ';
+		
+		$body ="From: $name\n E-Mail: $email\n Message:\n $message";
+		// Check if name has been entered
+		if (!$_POST['name']) {
+			$errName = 'Please enter your name';
+		}
+		
+		// Check if email has been entered and is valid
+		if (!$_POST['email'] || !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+			$errEmail = 'Please enter a valid email address';
+		}
+		
+		//Check if message has been entered
+		if (!$_POST['message']) {
+			$errMessage = 'Please enter your message';
+		}
+		//Check if simple anti-bot test is correct
+		if ($human !== 5) {
+			$errHuman = 'Your anti-spam is incorrect';
+		}
+// If there are no errors, send the email
+if (!$errName && !$errEmail && !$errMessage && !$errHuman) {
+	if (mail ($to, $subject, $body, $from)) {
+		$result='<div class="alert alert-success">Thank You! I will be in touch</div>';
+	} else {
+		$result='<div class="alert alert-danger">Sorry there was an error sending your message. Please try again later.</div>';
+	}
 }
+	}
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
-<head>
-   <meta charset="utf-8">
-   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-   <meta name="viewport" content="width=device-width, initial-scale=1">
-   <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
-   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>
-   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css">
-</head>
-<body>
-   <div class ="container">
-      <div class="row">
-         <div class="col-md-6 col-md-offset-3">
-            <form id="form-horizontal" method="POST" action="" role="form">
-
-               <div class="form-group">
-                  <label class="col-sm-2 control-label" for="name">Name</label>
-                  <div class="col-sm-10">
-                     <input class="form-control" type="text" name="name" placeholder="Name">
-                     <?php echo "<p class='text-danger'>$errorName</p>"; ?>
-                  </div>
-               </div>
-
-               <div class="form-group">
-                  <label class="col-sm-2 control-label" for="email">Email</label>
-                  <div class="col-sm-10">
-                     <input class="form-control" type="text" name="email" placeholder="Email">
-                     <?php echo "<p class='text-danger'>$errorEmail</p>"; ?>
-                  </div>
-               </div>
-
-               <div class="form-group">
-                  <label class="col-sm-2 control-label" for="message">Message</label>
-                  <div class="col-sm-10">
-                     <textarea class="form-control" rows="4" type="text" name="message" placeholder="Message"></textarea>
-                     <?php echo "<p class='text-danger'>$errorMessage</p>"; ?>
-                  </div>
-               </div>
-
-               <div class="form-group">
-                  <div class="col-sm-10 col-sm-offset-2">
-                     <input type="submit" class="btn btn-primary" name="submt" value="Submit">
-                  </div>
-               </div>
-
-               <div class="form-group">
-                  <div class="col-sm-10 col-sm-offset-2">
-                     <?php echo $result; ?>
-                  </div>
-               </div>
-
-            </form>
-         </div>
-      </div>
-   </div>
-</body>
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="Bootstrap contact form with PHP example by BootstrapBay.com.">
+    <meta name="author" content="BootstrapBay.com">
+    <title>Bootstrap Contact Form With PHP Example</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css">
+  </head>
+  <body>
+  	<div class="container">
+  		<div class="row">
+  			<div class="col-md-6 col-md-offset-3">
+  				<h1 class="page-header text-center">Contact Form Example</h1>
+				<form class="form-horizontal" role="form" method="post" action="index.php">
+					<div class="form-group">
+						<label for="name" class="col-sm-2 control-label">Name</label>
+						<div class="col-sm-10">
+							<input type="text" class="form-control" id="name" name="name" placeholder="First & Last Name" value="<?php echo htmlspecialchars($_POST['name']); ?>">
+							<?php echo "<p class='text-danger'>$errName</p>";?>
+						</div>
+					</div>
+					<div class="form-group">
+						<label for="email" class="col-sm-2 control-label">Email</label>
+						<div class="col-sm-10">
+							<input type="email" class="form-control" id="email" name="email" placeholder="example@domain.com" value="<?php echo htmlspecialchars($_POST['email']); ?>">
+							<?php echo "<p class='text-danger'>$errEmail</p>";?>
+						</div>
+					</div>
+					<div class="form-group">
+						<label for="message" class="col-sm-2 control-label">Message</label>
+						<div class="col-sm-10">
+							<textarea class="form-control" rows="4" name="message"><?php echo htmlspecialchars($_POST['message']);?></textarea>
+							<?php echo "<p class='text-danger'>$errMessage</p>";?>
+						</div>
+					</div>
+					<div class="form-group">
+						<label for="human" class="col-sm-2 control-label">2 + 3 = ?</label>
+						<div class="col-sm-10">
+							<input type="text" class="form-control" id="human" name="human" placeholder="Your Answer">
+							<?php echo "<p class='text-danger'>$errHuman</p>";?>
+						</div>
+					</div>
+					<div class="form-group">
+						<div class="col-sm-10 col-sm-offset-2">
+							<input id="submit" name="submit" type="submit" value="Send" class="btn btn-primary">
+						</div>
+					</div>
+					<div class="form-group">
+						<div class="col-sm-10 col-sm-offset-2">
+							<?php echo $result; ?>	
+						</div>
+					</div>
+				</form> 
+			</div>
+		</div>
+	</div>   
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>
+  </body>
 </html>
