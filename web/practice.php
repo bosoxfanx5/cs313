65f91514053532729334c7e7b433c20a655ca194
bosoxfanx5 <rob15047@byui.edu>
2017-02-22 22:07:37 -0800

    Update practice.php

--- web/practice.php
+++ web/practice.php
@@ -69,7 +69,9 @@ if (isset($_POST["submit"])) {
       </div>
     </div>
   </form>
-</body>
+
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+  </body>
+</html>
