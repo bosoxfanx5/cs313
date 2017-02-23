3ce8060400140a917eff5eaa4fe1fc5b90356fb5
bosoxfanx5 <rob15047@byui.edu>
2017-02-22 23:22:26 -0800

    Update practice.php

--- web/practice.php
+++ web/practice.php
@@ -54,7 +54,7 @@ if (!$errName && !$errEmail && !$errMessage && !$errHuman) {
   		<div class="row">
   			<div class="col-md-6 col-md-offset-3">
   				<h1 class="page-header text-center">Contact Form Example</h1>
-				<form class="form-horizontal" role="form" method="post" action="index.php">
+				<form class="form-horizontal" role="form" method="post" action="">
 					<div class="form-group">
 						<label for="name" class="col-sm-2 control-label">Name</label>
 						<div class="col-sm-10">
